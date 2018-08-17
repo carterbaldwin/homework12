240909ff8251be3d19b5ed2704f725ac70c3c5c1
Carter Baldwin <carterbaldwin@gmail.com>
2018-08-17 18:35:12 -0400

    full commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('cheeseburger');
+INSERT INTO burgers (name) VALUES ('bacon cheeseburger');
+-- INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
+-- INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
+-- INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
+-- INSERT INTO cats (name) VALUES ('Owens');
